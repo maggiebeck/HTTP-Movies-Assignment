1919b86200f7be81a866add6221c8af5a805007a
Margaret <mpbeck@umich.edu>
2019-08-22 19:07:39 -0400

    update form made

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { Route } from "react-router-dom";
 import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
+import MovieUpdate from "./Movies/UpdateMovie";
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -16,10 +17,13 @@ const App = () => {
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList} />
       <Route
+      
         path="/movies/:id"
         render={props => {
           return <Movie {...props} addToSavedList={addToSavedList} />;
         }}
+      />
+      <Route path="/update-movie/:id" component={MovieUpdate}
       />
     </>
   );
